980e7e7dfbcda7f49dc1cea8634d461023113165
RashmiBansode <rashmibansode86@gmail.com>
2022-10-21 16:31:25 +0530

    hello

---
+++ index.js
@@ -0,0 +1,10 @@
+let a = prompt('What is the official name of javascript?');
+let b = "ECMAScript";
+if (a == b) {
+   document.write('Answer is Correct');
+}
+else {
+   document.write('Answer is Incorrect');
+} 
+
+
